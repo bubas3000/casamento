27385cd15ee8436ec7296221bcdb06f49cf339ed
bubas3000 <47362581+bubas3000@users.noreply.github.com>
2024-06-09 17:10:19 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,8 @@
 
 <!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
-	<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Madalena e Afonso</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="description" content="Casamento Madalena e Afonso" />
 
-	
-	</head>
-	ent.getElementById("myForm1").style.display = "none";
+	<p>Madalena e Afonso</p>
+
 	
 </html>
 
